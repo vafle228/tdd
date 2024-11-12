5640931daaf290b60a01f4f092e88a3a83ceb63d
vafle228 <dimasmyslaev228@gmail.com>
2024-11-12 19:32:58 +0500

    Обновил структуру проекта

---
+++ cs/TagsCloudVisualization/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Rectangle rect = new Rectangle(10, 10, 50, 50);
+    }
+}
---
+++ cs/TagsCloudVisualizationTests/TagsCloudVisualizationTest.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualizationTests;
+
+[TestFixture]
+public class TagsCloudVisualizationTest
+{
+    [Test]
+    public void TagsCloudVisualizationTest123()
+    {
+        1.Should().Be(1);
+    }
+}
