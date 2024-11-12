e8bbaa436195591074d5f5f1aa7a93e6a32bf2a5
vafle228 <dimasmyslaev228@gmail.com>
2024-11-12 20:25:04 +0500

    Тест на инициализацию конструктора

---
+++ cs/TagsCloudVisualizationTests/CircularCloudLayouterTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudVisualization;
+
+namespace TagsCloudVisualizationTests;
+
+[TestFixture]
+public class CircularCloudLayouterTest
+{
+    public static IEnumerable<TestCaseData> InitCenterAtGivenPointTestCases
+    {
+        get
+        {
+            yield return new TestCaseData(new Point(0, 0));
+            yield return new TestCaseData(new Point(-10, 10));
+            yield return new TestCaseData(new Point(2000, 10000));
+        }
+    }
+    
+    [TestCaseSource(nameof(InitCenterAtGivenPointTestCases))]
+    public void CircularCloudLayouter_InitCenterAtGivenPoint(Point center)
+    {
+        var layouter = new CircularCloudLayouter(center);
+        
+        layouter.Center.Should().BeEquivalentTo(center);
+    }
+}
