da8665962d7db426821d8e698eb2274cb1d625b5
vafle228 <dimasmyslaev228@gmail.com>
2024-11-12 22:31:53 +0500

    Забыл про Direction ...

---
+++ cs/TagsCloudVisualization/SpiralCloudLayouter/Direction.cs
@@ -0,0 +1,24 @@
+﻿namespace TagsCloudVisualization.SpiralCloudLayouter;
+
+public enum Direction
+{
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+internal static class DirectionExtensions
+{
+    public static Direction AntiClockwiseRotate(this Direction direction)
+    {
+        return direction switch
+        {
+            Direction.Up => Direction.Left,
+            Direction.Left => Direction.Down,
+            Direction.Down => Direction.Right,
+            Direction.Right => Direction.Up,
+            _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
+        };
+    }
+}
