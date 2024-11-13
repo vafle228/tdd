b091b0989d03bab3e48b56caab661533336dad14
vafle228 <dimasmyslaev228@gmail.com>
2024-11-13 23:12:48 +0500

    Генерация точек - Добавил тест на генерацию точек - Реализовал генерацию

--- cs/TagsCloudVisualization/SpiralLayouter/PointGenerator/PolarArchimedesSpiral.cs
+++ cs/TagsCloudVisualization/SpiralLayouter/PointGenerator/PolarArchimedesSpiral.cs
@@ -5,42 +5,50 @@ namespace TagsCloudVisualization.SpiralLayouter.PointGenerator;
 
 public class PolarArchimedesSpiral : IPointGenerator<Point>
 {
+    private double currentAngle;
+    
     public Point Center { get; }
-    public double Angle { get; }
     public double Radius { get; }
+    public double AngleOffset { get; }
 
-    public Point Current { get; }
     object? IEnumerator.Current => Current;
+    public Point Current { get; private set; }
 
-    public PolarArchimedesSpiral(Point center, double radius, double angle)
+    public PolarArchimedesSpiral(Point center, double radius, double angleOffset)
     {
-        if (radius <= 0 || angle <= 0)
+        if (radius <= 0 || angleOffset <= 0)
         {
-            var argName = radius <= 0 ? nameof(radius) : nameof(angle);
+            var argName = radius <= 0 ? nameof(radius) : nameof(angleOffset);
             throw new ArgumentException("Spiral params should be positive.", argName);
         }
-            
-        
-        Angle = angle;
-        Radius = radius;
         
         Center = center;
         Current = center;
+        
+        Radius = radius;
+        AngleOffset = angleOffset * Math.PI / 180;
     }
     
     public IEnumerator<Point> GetEnumerator() => this;
     IEnumerator IEnumerable.GetEnumerator() => this;
-    
-    public double Step => Radius / (2 * Math.PI);
 
     public bool MoveNext()
     {
-        throw new NotImplementedException();
+        currentAngle += AngleOffset;
+        var polarCoordinate = Radius / (2 * Math.PI) * currentAngle;
+        
+        var xOffset = (int)Math.Round(polarCoordinate * Math.Cos(currentAngle));
+        var yOffset = (int)Math.Round(polarCoordinate * Math.Sin(currentAngle));
+        
+        Current = Center + new Size(xOffset, yOffset);
+        
+        return true;
     }
 
     public void Reset()
     {
-        throw new NotImplementedException();
+        Current = Center;
+        currentAngle = 0;
     }
     
 
--- cs/TagsCloudVisualizationTests/SpiralLayouter/PointGenerator/PolarArchimedesSpiralTest.cs
+++ cs/TagsCloudVisualizationTests/SpiralLayouter/PointGenerator/PolarArchimedesSpiralTest.cs
@@ -21,13 +21,13 @@ public class PolarArchimedesSpiralTest
     }
 
     [TestCaseSource(nameof(InitAtGivenPointAngleAndRadiusTestCases))]
-    public void PolarArchimedesSpiral_InitAtGivenPointAngleAndRadius(Point center, double radius, double angle)
+    public void PolarArchimedesSpiral_InitAtGivenPointAngleAndRadius(Point center, double radius, double angleOffset)
     {
-        var polarSpiral = new PolarArchimedesSpiral(center, radius, angle);
+        var polarSpiral = new PolarArchimedesSpiral(center, radius, angleOffset);
 
-        polarSpiral.Angle.Should().Be(angle);
         polarSpiral.Radius.Should().Be(radius);
         polarSpiral.Center.Should().BeEquivalentTo(center);
+        polarSpiral.AngleOffset.Should().Be(angleOffset * Math.PI / 180);
     }
     
     public static IEnumerable<TestCaseData> ThrowErrorOnNotPositiveNumberTestCases
@@ -41,13 +41,32 @@ public class PolarArchimedesSpiralTest
     }
 
     [TestCaseSource(nameof(ThrowErrorOnNotPositiveNumberTestCases))]
-    public void PolarArchimedesSpiral_ThrowError_OnNotPositiveNumber(Point center, double radius, double angle)
+    public void PolarArchimedesSpiral_ThrowError_OnNotPositiveNumber(Point center, double radius, double angleOffset)
     {
-        var negativeParameter = radius <= 0 ? nameof(radius) : nameof(angle);
-        var polarSpiralCtor = () => new PolarArchimedesSpiral(center, radius, angle);
+        var negativeParameter = radius <= 0 ? nameof(radius) : nameof(angleOffset);
+        var polarSpiralCtor = () => new PolarArchimedesSpiral(center, radius, angleOffset);
 
         polarSpiralCtor.Should()
             .Throw<ArgumentException>()
             .WithMessage($"{NOT_POSITIVE_ARGUMENT_ERROR} (Parameter '{negativeParameter}')");
+    }
+    
+        
+    [Test]
+    public void PolarArchimedesSpiral_CalculateSpecialPoints()
+    {
+        var polarSpiral = new PolarArchimedesSpiral(new Point(0, 0), 2, 360);
+        var expected = new[]
+        {
+            new Point(2, 0), new Point(4, 0), new Point(6, 0), new Point(8, 0)
+        };
+        
+        var expectedAndReceived = expected.Zip(polarSpiral);
+        
+        polarSpiral.Current.Should().BeEquivalentTo(new Point(0, 0));
+        foreach (var (expectedPoint, receivedPoint) in expectedAndReceived)
+        {
+            expectedPoint.Should().BeEquivalentTo(receivedPoint);
+        }
     }
 }
