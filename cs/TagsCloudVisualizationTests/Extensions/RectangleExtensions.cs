7a1456cce7575d0e7cff047a7cd54a0fb8832fed
vafle228 <dimasmyslaev228@gmail.com>
2024-11-18 00:41:03 +0500

    Тесты на плотность укладки

---
+++ cs/TagsCloudVisualizationTests/Extensions/RectangleExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualizationTests.Extensions;
+
+public static class RectangleExtensions
+{
+    public static double Area(this Rectangle rectangle)
+    {
+        return rectangle.Width * rectangle.Height;
+    }
+    
+}
--- cs/TagsCloudVisualizationTests/SpiralLayouter/SpiralCloudLayouterTest.cs
+++ cs/TagsCloudVisualizationTests/SpiralLayouter/SpiralCloudLayouterTest.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 using TagsCloudVisualization;
 using TagsCloudVisualization.SpiralLayouter;
 using TagsCloudVisualization.SpiralLayouter.PointGenerator;
+using TagsCloudVisualizationTests.Extensions;
 
 namespace TagsCloudVisualizationTests.SpiralLayouter;
 
@@ -26,6 +27,15 @@ public class SpiralCloudLayouterTest
         {
             yield return new TestCaseData(new SquareArchimedesSpiral(5));
             yield return new TestCaseData(new PolarArchimedesSpiral(10, 10));
+        }
+    }
+    
+    private static IEnumerable<TestCaseData> LayingDensityTestCase
+    {
+        get
+        {
+            yield return new TestCaseData(new SquareArchimedesSpiral(5), 0.75);
+            yield return new TestCaseData(new PolarArchimedesSpiral(10, 5), 0.65);
         }
     }
     
@@ -66,6 +76,19 @@ public class SpiralCloudLayouterTest
         intersectedRect.Should().Throw<InvalidOperationException>("No intersection found");
     }
 
+    [TestCaseSource(nameof(LayingDensityTestCase))]
+    [Repeat(10)]
+    public void SpiralCloudLayouter_PutNextRectangle_SatisfyDensityMinimum(IPointGenerator pointGenerator, double min)
+    {
+        var layouter = new SpiralCloudLayouter(new Point(0, 0), pointGenerator);
+        var rectangles = PlaceRectangles(50, layouter);
+
+        var claimedArea = FindClaimedRectangle(rectangles).Area();
+        var totalArea = rectangles.Select(r => r.Area()).Sum();
+
+        (totalArea / claimedArea).Should().BeGreaterThan(min);
+    }
+
     private List<Rectangle> PlaceRectangles(int count, ICloudLayouter layouter)
     {
         var rectangleSizes = Enumerable.Range(0, count)
@@ -78,5 +101,16 @@ public class SpiralCloudLayouterTest
     {
         var random = new Random();
         return new Size(random.Next(min, max), random.Next(min, max));
+    }
+
+    private Rectangle FindClaimedRectangle(List<Rectangle> rectangles)
+    {
+        var leftX = rectangles.Min(r => r.Location.X);
+        var leftY = rectangles.Min(r => r.Location.Y);
+        
+        var width = rectangles.Max(r => r.Location.X) - leftX;
+        var height = rectangles.Max(r => r.Location.Y) - leftY;
+        
+        return new Rectangle(leftX, leftY, width, height);
     }
 }
